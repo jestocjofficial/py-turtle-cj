76a515ab585c03f9a3e0b4868af900ebaa5ea5c4
JESTO CJ <jestocjofficial@gmail.com>
2022-08-08 23:48:48 +0530

    Create cj.py

---
+++ cj.py
@@ -0,0 +1,142 @@
+from turtle import *
+
+bgcolor("black")
+
+pensize(3)
+
+goto(-55, -55)
+
+for i in range(1):
+
+    pencolor("yellow")
+
+    color("yellow", "orange")
+
+    begin_fill()
+
+    rt(180)
+
+    fd(80)
+
+    rt(90)
+
+    fd(150)
+
+    rt(90)
+
+    fd(110)
+
+    rt(90)
+
+    fd(30)
+
+    rt(90)
+
+    fd(80)
+
+    lt(90)
+
+    fd(90)
+
+    lt(90)
+
+    fd(80)
+
+    rt(90)
+
+    fd(30)
+
+    rt(90)
+
+    fd(50)
+
+    lt(180)
+
+    fd(50)
+
+    end_fill()
+
+    pencolor("black")
+
+    fd(50)
+
+    pencolor("yellow")
+
+    color("orange", "red")
+
+    begin_fill()
+
+    lt(90)
+
+    fd(30)
+
+    rt(90)
+
+    fd(50)
+
+    lt(90)
+
+    fd(90)
+
+    lt(90)
+
+    fd(50)
+
+    rt(90)
+
+    fd(30)
+
+    rt(90)
+
+    fd(110)
+
+    rt(90)
+
+    fd(30)
+
+    rt(90)
+
+    fd(30)
+
+    lt(90)
+
+    fd(120)
+
+    rt(90)
+
+    fd(80)
+
+    end_fill()
+
+    pencolor("black")
+
+    lt(90)
+
+    fd(40)
+
+    pencolor("white")
+
+    pensize(12)
+
+    rt(90)
+
+    fd(190)
+
+    rt(90)
+
+    fd(230)
+
+    rt(90)
+
+    fd(330)
+
+    rt(90)
+
+    fd(230)
+
+    rt(90)
+
+    fd(230)
+
+exitonclick()
+
